3624266b8dd2d12918a146eb4c800abb0e02dc1b
Tammy Husnetdinova <43862224+t-husnetdinova@users.noreply.github.com>
2021-03-18 20:12:34 -0600

    added files from previous classwork

---
+++ main.js
@@ -0,0 +1,34 @@
+const express = require("express"), app = express();
+homeController = require("./controllers/homeController");
+layouts = require("express-ejs-layouts");
+errorController = require("./controllers/errorController");
+
+app.set("port", process.env.PORT || 3000);
+
+app.set("view engine", "ejs");
+app.use(layouts);
+
+app.get("/", homeController.showIndex);
+
+app.use(express.static("public"));
+
+app.use(
+    express.urlencoded({
+        extended: false
+    })
+);
+
+app.use(express.json());
+
+// routes 
+app.get("/courses", homeController.showCourses);
+app.get("/contact", homeController.showSignUp);
+app.post("/contact", homeController.postedSignUpForm); 
+
+// error handling
+app.use(errorController.pageNotFoundError);
+app.use(errorController.internalServerError);
+
+app.listen(app.get("port"), () => {
+    console.log(`Server is running on port: ${app.get("port")}`)
+});
